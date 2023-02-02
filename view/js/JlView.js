19bab474b9dd9688fa040f5d4d055c6e82c0560a
GrupoToberto <jlsystemsmex@hotmail.com>
2023-02-02 10:29:57 -0600

    Se agregan controladoras para js

---
+++ view/js/JlView.js
@@ -0,0 +1,58 @@
+class JlView{
+    #id;
+
+    constructor(id){
+        if (this.constructor == JlView)
+			throw new Error("Abstract classes can't be instantiated.");
+
+        this.#id=id;
+    }
+
+    getId(){
+        return this.#id;
+    }
+
+    findViewById(id){
+        var tag=document.getElementById(this.#id+id).tagName.toLowerCase();
+        tag=tag[0].toUpperCase()+tag.substring(1);
+        return eval('new Jl'+tag+'("'+id+'");');
+    }
+}
+
+class JlDiv extends JlView{
+    constructor(id){
+        super(id);
+    }
+}
+
+class JlTextTag extends JlView{
+    #text;
+
+    constructor(id){
+        super(id);
+
+        if (this.constructor == JlTextTag)
+			throw new Error("Abstract classes can't be instantiated.");
+
+        this.#text='';
+    }
+
+    setText(text){
+        this.#text=text;
+
+        var textTag=document.getElementById(super.getId());
+		textTag.innerHTML=this.#text;
+    }
+}
+
+class JlH5 extends JlTextTag{
+    constructor(id){
+        super(id);
+    }
+}
+
+class JlParagraph extends JlTextTag{
+    constructor(id){
+        super(id);
+    }
+}
