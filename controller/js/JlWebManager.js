b0181d2fe0bfe06a7eb9ccbc60306f8c7526eb5b
GrupoToberto <103981508+GrupoToberto@users.noreply.github.com>
2023-02-03 00:06:48 -0600

    Update JlWebManager.js

--- controller/js/JlWebManager.js
+++ controller/js/JlWebManager.js
@@ -9,31 +9,41 @@ class JlWebManager{
 
     static includeHTML(){ //Gotten by w3schools
         var z, i, elmnt, file, xhttp;
+        var n;
         /* Loop through a collection of all HTML elements: */
         z = document.getElementsByTagName("*");
         for (i = 0; i < z.length; i++) {
             elmnt = z[i];
             /*search for elements with a certain atrribute:*/
             file = elmnt.getAttribute("jlInclude");
-            if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                if (this.status == 200) {
-                    elmnt.innerHTML = this.responseText.replaceAll('id="', 'id="'+elmnt.id);
-                }
-                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                /* Remove the attribute, and call this function once more: */
-                elmnt.removeAttribute("jlInclude");
-                JlWebManager.includeHTML();
-                JlWebManager.onHTML_Included();
-                }
+            n=1;
+
+            if(!file){
+                file = elmnt.getAttribute("jlRepeat").split(" ")[0];
+                n = eval(elmnt.getAttribute("jlRepeat").split(" ")[2]+".length");
             }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
+
+            for(let j=0; j<n; j++){
+                if (file) {
+                    /* Make an HTTP request using the attribute value as the file name: */
+                    xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function() {
+                        if (this.readyState == 4) {
+                        if (this.status == 200) {
+                            elmnt.innerHTML = this.responseText.replaceAll('id="', 'id="'+elmnt.id);
+                        }
+                        if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                        /* Remove the attribute, and call this function once more: */
+                        elmnt.removeAttribute("jlInclude");
+                        JlWebManager.includeHTML();
+                        JlWebManager.onHTML_Included();
+                        }
+                    }
+                    xhttp.open("GET", file, true);
+                    xhttp.send();
+                    /* Exit the function: */
+                    return;
+            }
     }
   }
     }
