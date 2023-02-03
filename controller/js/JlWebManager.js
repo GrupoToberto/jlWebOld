d5e7adf042b7daa2cc3585ce08d7f30e1392cf76
“JlSystemsMex” <jlsystemsmex@hotmail.com>
2023-02-03 00:40:00 -0600

    Ajuste

--- controller/js/JlWebManager.js
+++ controller/js/JlWebManager.js
@@ -9,7 +9,7 @@ class JlWebManager{
 
     static includeHTML(){ //Gotten by w3schools
         var z, i, elmnt, file, xhttp;
-        var n;
+        var n, s;
         /* Loop through a collection of all HTML elements: */
         z = document.getElementsByTagName("*");
         for (i = 0; i < z.length; i++) {
@@ -24,14 +24,18 @@ class JlWebManager{
                 n = file ? eval(elmnt.getAttribute("jlRepeat").split(" ")[2]+".length") : 0;
             }
 
-            for(let j=0; j<n; j++){
                 if (file) {
                     /* Make an HTTP request using the attribute value as the file name: */
                     xhttp = new XMLHttpRequest();
                     xhttp.onreadystatechange = function() {
                         if (this.readyState == 4) {
                         if (this.status == 200) {
-                            elmnt.innerHTML = this.responseText.replaceAll('id="', 'id="'+elmnt.id);
+                            s='';
+                            for(let j=0; j<n; j++)
+                                s+=n==1?this.responseText.replaceAll('id="', 'id="'+elmnt.id):this.responseText.replaceAll('id="', 'id="'+elmnt.id+j);
+                                
+
+                            elmnt.innerHTML = s;
                         }
                         if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
                         /* Remove the attribute, and call this function once more: */
@@ -44,7 +48,7 @@ class JlWebManager{
                     xhttp.send();
                     /* Exit the function: */
                     return;
-            }
+            
     }
   }
     }
